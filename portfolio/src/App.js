9b245aeaafa1303494086d5a4cfce91d45f7420f
the-datageek <moraaf770@gmail.com>
2023-06-06 14:31:28 +0300

    Initial commit

---
+++ portfolio/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import Projects from './components/Projects/projects';
+
+function App() {
+  return (
+    <div className="App">
+      <Projects/>
+    </div>
+  );
+}
+
+export default App;
